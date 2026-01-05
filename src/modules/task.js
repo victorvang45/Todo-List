a418a5cc1a032682b4c969047bcf24cdf53a6a06
victorvang45 <victorvang456@gmail.com>
2026-01-05 11:06:10 -0600

    Created Task Class

---
+++ src/modules/task.js
@@ -0,0 +1,53 @@
+class Task {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    // getter methods
+
+    getTitle() {
+        return this.title;
+    }
+
+    getDescription() {
+        return this.description;
+    }
+
+    getDueDate() {
+        return this.dueDate;
+    }
+
+    getPriority() {
+        return this.priority;
+    }
+
+
+    // setter methods
+
+    setTitle(title) {
+        this.title = title;
+    }
+
+    setDescription(description) {
+        this.description = this.description;
+    }
+
+    setDueDate(dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    setPriority(priority) {
+        this.priority = priority;
+    }
+
+    // Formatting for dates
+    getDateFormatted() {
+        const day = this.dueDate.split('/')[0]
+        const month = this.dueDate.split('/')[1]
+        const year = this.dueDate.split('/')[2]
+        return `${month}/${day}/${year}`
+    }
+}
